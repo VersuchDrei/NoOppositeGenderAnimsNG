918ef16c4f4754902ee3a65ca1458304af2db14a
VersuchDrei <FalkoMitC@gmail.com>
2023-09-26 09:08:01 +0200

    implemented

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,8 +16,7 @@ namespace {
 
         std::shared_ptr<spdlog::logger> log;
         if (IsDebuggerPresent()) {
-            log = std::make_shared<spdlog::logger>(
-                "Global", std::make_shared<spdlog::sinks::msvc_sink_mt>());
+            log = std::make_shared<spdlog::logger>("Global", std::make_shared<spdlog::sinks::msvc_sink_mt>());
         } else {
             log = std::make_shared<spdlog::logger>(
                 "Global", std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true));
@@ -28,6 +27,18 @@ namespace {
         spdlog::set_default_logger(std::move(log));
         spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
     }
+
+    void MessageHandler(SKSE::MessagingInterface::Message* a_msg) {
+        switch (a_msg->type) {
+            case SKSE::MessagingInterface::kDataLoaded: {
+                RE::TESDataHandler* handler = RE::TESDataHandler::GetSingleton();
+                auto& npcs = handler->GetFormArray<RE::TESNPC>();
+                for (RE::TESNPC* npc : npcs) {
+                    npc->actorData.actorBaseFlags.reset(RE::ACTOR_BASE_DATA::Flag::kOppositeGenderanims);
+                }
+            }
+        }
+    }
 }
 
 SKSEPluginLoad(const LoadInterface* skse) {
@@ -37,8 +48,12 @@ SKSEPluginLoad(const LoadInterface* skse) {
     auto version = plugin->GetVersion();
     log::info("{} {} is loading...", plugin->GetName(), version);
 
-
     Init(skse);
+
+    auto message = SKSE::GetMessagingInterface();
+    if (!message->RegisterListener(MessageHandler)) {
+        return false;
+    }
 
     log::info("{} has finished loading.", plugin->GetName());
     return true;
